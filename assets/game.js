e13d896838ea3606ef79ad29033984fc87413b43
Rachel Cutler <rach_cutler@live.co.uk>
2023-12-13 15:13:13 +0000

    added calss to change answer colour depending on if it is correct or incorrect

--- assets/game.js
+++ assets/game.js
@@ -1,9 +1,8 @@
 const question = document.getElementById("question");
-const choices = Array.from(document.getElementsByClassName("choices"));
-console.log(choices);
+const choices = Array.from(document.getElementsByClassName("choice-text"));
 
 let currentQuestion = {};
-let acceptingAnswers = true;
+let acceptingAnswers = false;
 let score = 0;
 let questionCounter = 0;
 let availableQuestions = [];
@@ -36,6 +35,7 @@ let questions = [
     },
 ];
 
+// CONSTANTS
 const CORRECT_BONUS = 10;
 const MAX_QUESTIONS = 3;
 
@@ -48,15 +48,47 @@ startGame = () => {
 };
 
 getNewQuestion = () => {
+
+    if(availableQuestions.length === 0 || questionCounter >= MAX_QUESTIONS) {
+        //go to the end page
+        return window.location.assign("/end.html");
+    }
     questionCounter++;
     const questionIndex = Math.floor(Math.random() * availableQuestions.length);
     currentQuestion = availableQuestions[questionIndex];
     question.innerText = currentQuestion.question;
 
     choices.forEach((choice) => {
-        const number = choice.dataset ['number'];
+        const number = choice.dataset['number'];
         choice.innerText = currentQuestion['choice' + number];
     });
+
+    // This removes the question from the list so its not repeated
+    availableQuestions.splice(questionIndex, 1);
+    acceptingAnswers = true;
 };
+
+choices.forEach(choice => {
+    choice.addEventListener("click", e => {
+        if(!acceptingAnswers) return;
+
+        acceptingAnswers = false;
+        const selectedChoice = e.target;
+        const selectedAnswer = selectedChoice.dataset["number"];
+
+        let classToApply = "incorrect";
+            if (selectedAnswer == currentQuestion.answer) {
+                classToApply = "correct";
+            }
+        // apply this class to the text box not just the text
+        selectedChoice.parentElement.classList.add(classToApply);
+        
+        // this removes class an gets new question after delay 
+        setTimeout(() => {
+            selectedChoice.parentElement.classList.remove(classToApply);
+            getNewQuestion();
+        }, 1000);
+    });
+});
 
 startGame();
